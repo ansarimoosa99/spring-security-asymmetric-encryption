d9b9ccbb336e021cbc9222fbce681dfa7b70ef05
Moosa Ansari <ansarimoosa99@gmail.com>
2025-07-24 17:07:11 +0530

    added AuthServiceImpl

--- src/main/java/com/ansari/app/auth/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/ansari/app/auth/impl/AuthenticationServiceImpl.java
@@ -7,20 +7,23 @@ import com.ansari.app.auth.request.RegistrationRequest;
 import com.ansari.app.auth.response.AuthenticationResponse;
 import com.ansari.app.exception.BusinessException;
 import com.ansari.app.exception.ErrorCode;
+import com.ansari.app.role.Role;
 import com.ansari.app.role.RoleRepository;
 import com.ansari.app.security.JwtService;
 import com.ansari.app.user.User;
 import com.ansari.app.user.UserMapper;
 import com.ansari.app.user.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -67,32 +70,62 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         checkUserPhoneNumber(request.getPhoneNumber());
         checkPassword(request.getPassword(), request.getConfirmPassword());
 
+        //fetch the user role from the database
+        final Role userRole = this.roleRepository.findByName("ROLE_USER")
+                .orElseThrow(() -> new EntityNotFoundException("Role user does not exist"));
+
+        final List<Role> roles = new ArrayList<>();  // we have many roles, so we need to create a list of roles
+        roles.add(userRole);
+
+        final User user = this.userMapper.toUser(request);  // map the request to user entity
+        user.setRoles(roles);
+        log.debug("Saving user {}", user);
+        this.userRepository.save(user);  // save the user to the database
+
+        //this should be enouughh-----
+
+        //what we need to do afterward is to save or persist the role  itself,  ////but since we are using CascadeType.PERSIST and CascadeType.MERGE in the User entity, it will automatically save the roles when we save the user.
+        // we need to assisgn user to the user role as we have many to many relationship
+        // not needed[as in Roles we have manytoMany cascade.persist/merge] but still explained in case we dont have cascade.persist/merge
+        //how you need to deal w that.
+        final List<User> users = new ArrayList<>();
+        users.add(user);
+        userRole.setUsers(users);
+        this.roleRepository.save(userRole);
+        // asssign user to user role in case cascading not use;; this will save the user role and assign the user to the user role
 
     }
 
 
     @Override
     public AuthenticationResponse refresh(RefreshRequest request) {
-        return null;
+        final String newAccessToken = this.jwtService.refreshToken(request.getRefreshToken());
+        final String TokenType = "Bearer";
+
+        return AuthenticationResponse.builder()
+                .accessToken(newAccessToken)
+                .refershToken(request.getRefreshToken())
+                .tokenType(TokenType)
+                .build();
     }
 
 
     private void checkUserEmail(final String email) {
         final boolean emialExists = this.userRepository.existsByEmailIgnoreCase(email);
-        if(emialExists){
+        if (emialExists) {
             throw new BusinessException(ErrorCode.EMAIL_ALREADY_EXISTS);
         }
     }
 
     private void checkUserPhoneNumber(final String phoneNumber) {
         final boolean phoneNumberExists = this.userRepository.existsByPhoneNumber(phoneNumber);
-        if(phoneNumberExists){
+        if (phoneNumberExists) {
             throw new BusinessException(ErrorCode.PHONE_ALREADY_EXISTS);
         }
     }
 
     private void checkPassword(final String password, final String confirmPassword) {
-        if(password != null && password.equals(confirmPassword)){
+        if (password != null && password.equals(confirmPassword)) {
             throw new BusinessException(ErrorCode.PASSWORD_MISMATCH);
         }
 
--- src/main/java/com/ansari/app/user/UserMapper.java
+++ src/main/java/com/ansari/app/user/UserMapper.java
@@ -1,6 +1,6 @@
 package com.ansari.app.user;
 
-import ch.qos.logback.core.util.StringUtil;
+import com.ansari.app.auth.request.RegistrationRequest;
 import com.ansari.app.user.request.ProfileUpdateRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
@@ -8,19 +8,34 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserMapper {
     public void mergeUserInfo(final User user, final ProfileUpdateRequest request) {
-        if(StringUtils.isNotBlank(request.getFirstName())
-                && !user.getFirstName().equals(request.getFirstName())){
+        if (StringUtils.isNotBlank(request.getFirstName())
+                && !user.getFirstName().equals(request.getFirstName())) {
             user.setFirstName(request.getFirstName());
         }
 
-        if(StringUtils.isNotBlank(request.getLastName())
-                && !user.getLastName().equals(request.getLastName())){
+        if (StringUtils.isNotBlank(request.getLastName())
+                && !user.getLastName().equals(request.getLastName())) {
             user.setLastName(request.getLastName());
         }
 
-        if(request.getDateOfBirth() != null
-                && !user.getDateOfBirth().equals(request.getDateOfBirth())){
+        if (request.getDateOfBirth() != null
+                && !user.getDateOfBirth().equals(request.getDateOfBirth())) {
             user.setDateOfBirth(request.getDateOfBirth());
         }
+    }
+
+    public User toUser(RegistrationRequest request) {
+        return new User().builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .phoneNumber(request.getPhoneNumber())
+                .password(request.getPassword())
+                .enabled(true)
+                .locked(false)
+                .credentialsExpired(false)
+                .emailVerified(false)
+                .phoneVerified(false)
+                .build();
     }
 }
